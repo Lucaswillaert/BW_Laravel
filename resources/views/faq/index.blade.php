e04081d47b6e13fa5789e516d711f0792489fceb
Lucaswillaert <lucas.willaert@student.ehb.be>
2023-12-26 14:48:52 +0100

    Add About Us, FAQ, and Question Form sections to FAQ index page

--- resources/views/faq/index.blade.php
+++ resources/views/faq/index.blade.php
@@ -11,8 +11,37 @@
                                 {{ session('status') }}
                             </div>
                         @endif
-                       <!-- CONTENT PROFILE PAGE -->
-                       
+
+                        <!-- About Us Section -->
+                        <section id="about-us">
+                            <h2>About Us</h2>
+                            <p>...</p>
+                        </section>
+
+                        <!-- FAQ Section -->
+                        <section id="faq">
+                            <h2>FAQ</h2>
+                            <!-- Loop through FAQs -->
+                            @foreach($faqs as $faq)
+                                <details>
+                                    <summary>{{ $faq->question }}</summary>
+                                    <p>{{ $faq->answer }}</p>
+                                </details>
+                            @endforeach
+                        </section>
+
+                        <!-- Question Form Section -->
+                        <section id="question-form">
+                            <h2>Ask a Question</h2>
+                            <form method="POST" action="{{ route('questions.store') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <label for="question">Question</label>
+                                    <textarea class="form-control" id="question" name="question" rows="3"></textarea>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </form>
+                        </section>
                     </div>
                 </div>
             </div>
