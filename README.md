dbece6d38f0b7b8aa5842465cfbaed80f13f6fb9
Lucaswillaert <lucas.willaert@student.ehb.be>
2024-01-08 23:10:00 +0100

    Refactor controllers, fix bugs, and update routes

--- README.md
+++ README.md
@@ -1,74 +1,31 @@
 # BW_Laravel
  backend web 2IT
 
- MODELS 
- -------
- Users
-    -id
-    - username 
-    - email 
-    - password 
-    - is_admin 
-    -timestamps
-    -about
-Quotes 
-    -id
-    -message
-    -user_id (weten wie post en eigen post niet gaan liken )
-    -timestamps
-Likes 
-    -id 
-    -post_id
-    -user_id 
-FAQ 
-    - ID 
-    - Timestamp 
-    - Question 
-    - Answer 
-contact 
-    - ID 
-    - Username/email 
-    - info 
-
-entries 
-    - ID
-    -message
-    -Timestampe
-    -Title 
-
-CONTROLLERS
-----------
-UserController
-    - weergeven van profiel 
-    - profiel te editen 
-PostsController
-    + weergeven van alle posts x
-    - weergeven van form voor nieuwe post aan te maken x
-    - nieuwe post saven 
-    - weergeven van form voor post te editen 
-    - verwerken van editen 
-    - afhandelen van een delete 
-    - specifieke posts weergeven 
-LikeController 
-    - user kan post liken 
-aboutController 
-    - info over de website 
-    - FAQ staat erin waarmee de vragen die gesteld worden een antwoord op is 
-    - contact form om te sturen naar de admin 
-
-
-AdminPage 
-    - gebruikers 
-
-
-12min ! 
-
-
 wat nog te doen: 
-    - bij inlog en uitlog word altijd doorgerefereert naar /dashboard --> dit afzetten 
-    - nachecken of journalposts wel blijven 
-    - quote posts --> openbaar maken voor elke user en een like knop voorzien en een comment knop 
+    - quote posts --> van jongste naar oudste zetten x
+    - profile page kunnen bewerken en erin zetten dat ook img kan 
+        -linken naar name edit, about yourself edit en edit password 
+        - en eronder een lijst van alle quotes dat die user heeft geschreven in zo een "posts style" 
+        
+    - list of quostions and answers, gegroupeert in een categorie 
+        - privacy questions 
+        - Quotes questions
+    - seeders toevoegen  
+
+bugs te fixen: 
+- Register geeft door naar /dashboard 
+- logout moet ik dubbel klikken 
+- FAQ wil niet meer doorkomen de antwoorden 
+-login logo aanpassen
+- contact form klikken en naar toe gaan admin page 
+- search: als ik op de user click dan gaat die niet naar dat profiel 
 
 
-    - profile page kunnen bewerken en erin zetten dat ook img kan
-    - faq voor vragen en eronder een formulier voor een FAQ in te sturen 
+about page waar ik dat in moet zetten: 
+* https://laravel.com/docs/10.x 
+* https://www.youtube.com/playlist?list=PLFHz2csJcgk_mM2jEf7t8P678O_jz83on
+* https://tailwindcss.com
+* https://www.tutorialspoint.com/laravel/laravel_routing.html
+* https://fontawesome.com/icons/eye?f=classic&s=regular 
+* https://zenquotes.io 
+* https://www.youtube.com/watch?v=2mqsVzgsV_c&t=1979s
--- routes/web.php
+++ routes/web.php
@@ -18,42 +18,28 @@ use App\Http\Controllers\LikeController;
 use App\Http\Controllers\ContactController;
 use App\Models\Contact;
 
-/*
-GET - Request a resource
-POST - Create a new resource
-PUT - Update a resource (everything)
-PATCH - Update a resource (partial update)
-DELETE - Delete a resource
-OPTIONS - Get the supported HTTP methods
-*/
 //ROUTES VOOR POSTS
 Route::get('/', [PostController::class,'index'])->name('index');
-
-Route::get('posts', [PostController::class,'index']) ->middleware('auth') ->name('posts.index');
-
+Route::get('/posts', [PostController::class,'index']) ->middleware('auth') ->name('posts.index');
 Route::get('posts/create', [PostController::class,'create']) ->middleware('auth') ->name('posts.create');
-
 Route::post('posts', [PostController::class,'store']) ->middleware('auth') ->name('posts.store');
-
 //ROUTES COMMENTS/LIKES
 Route::post('posts/{post}/comments', [CommentController::class,'store'])->name('comments.store');
 Route::post('posts/{post}/like', [LikeController::class,'store'])->name('likes.store');
-
 Route::post('/posts/{post}/likes', [App\Http\Controllers\PostController::class, 'like'])->name('posts.like');
-
+//ROUTES VOOR JOURNAL
 //route voor het aanmaken van een entry
 Route::get('journal.create', [EntryController::class,'create']) ->middleware('auth') ->name('journal.create');
 //route voor het opslaan van een entry
 Route::post('journal', [EntryController::class,'store']) ->middleware('auth') ->name('journal.store');
 //route voor het openen van een entry 
 Route::get('journal/{id}', [EntryController::class,'show']) ->middleware('auth') ->name('journal.show');
-
 //ROUTES VOOR FAQ 
 Route::get('faq', [FaqController::class, 'index']) -> name('faq.index');
 //Routes voor het sturen van een question 
 Route::post('faq', [FaqController::class, 'store'])->name('faq.store');
 
- //ROUTES VOOR ABOUT
+//ROUTES VOOR ABOUT
 Route::get('about', [AboutController::class,'index']) ->name('about.index');
 //ROUTES VOOR CONTACT
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
@@ -67,14 +53,13 @@ Route::delete('admin/posts/{post}', [AdminController::class,'destroy'])->middlew
 Route::get('admin/contacts', [ContactController::class,'index'])->middleware('auth') ->name('admin.contacts');
 //FAQ ADMIN PAGE SUBMISSION
 Route::post('/faqs/{faq}/publish', [App\Http\Controllers\FaqController::class, 'publish'])->middleware('auth') ->name('faqs.publish');
-//FAQ ADMIN ANSWERs 
+//FAQ ADMIN ANSWERS
 Route::get('/faqs/{faq}/answer', [FaqController::class, 'answer'])->middleware('auth')->name('faqs.answer');
 //FAQ ADMIN FAQ ANSWER SUBMISSION
 Route::post('/faqs/{faq}/answer', [FaqController::class, 'storeAnswer'])->middleware('auth')->name('faqs.storeAnswer');
 //FAQ ADMIN DELETE FAQ
 Route::delete('/faqs/{faq}', [FaqController::class, 'destroy'])->middleware('auth')->name('faqs.destroy');
-//logout
-Route::post('auth/login', [LoginController::class,'logout'])->name('logout');
+
 
 // Register routes
 Route::get('register', [RegisterController::class,'view'])->name('register');
@@ -82,6 +67,8 @@ Route::get('register', [RegisterController::class,'view'])->name('register');
 Route::post('register', [RegisterController::class,'register']);
 //ROUTES VOOR LOGIN
 Route::get('auth/login', [LoginController::class,'view']) ->middleware('auth') ->name('login.view');
+//logout
+Route::post('auth/login', [LoginController::class,'logout'])->name('logout');
 
 //ROUTES VOOR ENTRIES 
 Route::get('journal.index', [EntryController::class,'index']) ->middleware('auth') ->name('journal.index');
