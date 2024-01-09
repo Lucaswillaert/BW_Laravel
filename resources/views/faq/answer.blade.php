868b0a4e2056f92232142481903b7775e53c2976
Lucaswillaert <lucas.willaert@student.ehb.be>
2024-01-09 23:46:24 +0100

    Update FAQ answer form

--- resources/views/faq/answer.blade.php
+++ resources/views/faq/answer.blade.php
@@ -14,9 +14,9 @@
                         <div class="post-container mx-auto bg-white rounded shadow-md mt-6 px-4 py-4 w-1/2">
 
                             <div class="container mx-auto px-6 py-8 lg:py-16">
-                                <h2
-                                    class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
-                                    Contact Us</h2>
+                                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+                                answer the question     
+                                </h2>
                                     <form method="POST" action="{{ route('faqs.storeAnswer', $faq->id) }}" class="space-y-8">
                                     @csrf
                                    
@@ -30,7 +30,7 @@
                                         <label for="message"
                                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your
                                             message</label>
-                                        <textarea id="message" name="message" rows="6"
+                                        <textarea id="answer" name="answer" rows="6"
                                             class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                             value = "{{$faq->answer}}"></textarea>
                                     </div>
